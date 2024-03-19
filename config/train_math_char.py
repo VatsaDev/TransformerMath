55d56245c31bb51487995c595520658dac18a396
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-03-18 21:51:35 -0500

    Update train_math_char.py

--- config/train_math_char.py
+++ config/train_math_char.py
@@ -25,8 +25,8 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 2000
+lr_decay_iters = 2000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
