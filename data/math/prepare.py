31313ac05bffb4b2daaca7344f63e51c88c9052c
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-03-21 11:15:59 -0500

    Update prepare.py

--- data/math/prepare.py
+++ data/math/prepare.py
@@ -1,9 +1,3 @@
-"""
-Prepare the Shakespeare dataset for character-level language modeling.
-So instead of encoding with GPT-2 BPE tokens, we just map characters to ints.
-Will save train.bin, val.bin containing the ids, and meta.pkl containing the
-encoder and decoder and some other related info.
-"""
 import os
 import pickle
 import requests
